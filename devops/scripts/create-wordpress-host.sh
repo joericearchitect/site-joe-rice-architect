81cc9da409e46ced2867a489f5a8c3cadfe1702e
Joe Rice <joericearchitect@gmail.com>
2016-10-14 22:08:05 +0000

    changed name of docker-machine.  called it 'sites' instead of 'blog-wordpress' cuz it's going to run multiple sites

--- devops/scripts/create-wordpress-host.sh
+++ devops/scripts/create-wordpress-host.sh
@@ -9,4 +9,4 @@
 #       export AWS_SECRET_ACCESS_KEY=<Super_Top_Secret>
 # *********************************************************************************
 
-docker-machine create -d amazonec2 --amazonec2-vpc-id=vpc-64260003 --amazonec2-region us-east-1 --amazonec2-instance-type "t2.micro" jra-blog-wordpress
+docker-machine create -d amazonec2 --amazonec2-vpc-id=vpc-64260003 --amazonec2-region us-east-1 --amazonec2-instance-type "t2.micro" jarch-sites
