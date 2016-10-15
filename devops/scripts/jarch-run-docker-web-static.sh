0ab79188753f3526d20a41a057ecaff010f15733
Joe Rice <joericearchitect@gmail.com>
2016-10-15 00:59:14 -0400

    script change

---
+++ devops/scripts/jarch-run-docker-web-static.sh
@@ -0,0 +1,10 @@
+# *********************************************************************************
+# Description: Builds a docker image for this node app
+# Author:      Joe Rice
+# Created:     10/14/2016
+#
+# Notes:
+#    Uses the Dockerfile in this same git repo
+# *********************************************************************************
+
+docker run -p 8080:8080 -d jarch/site-joe-rice-architecture-static
