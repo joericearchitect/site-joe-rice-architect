7fde4278c058a0cc671c917d971245a9faee1779
Joe Rice <joericearchitect@gmail.com>
2016-10-15 07:52:08 -0400

    small script fixes

--- devops/scripts/jarch-build-docker-image.sh
+++ devops/scripts/jarch-build-docker-image.sh
@@ -15,3 +15,7 @@ NAMESPACE=jarch
 IMAGE_NAME=site-jarch-static-web
 
 docker build -t $NAMESPACE/$IMAGE_NAME .
+
+docker tag jarch/site-jarch-static-web 193862077204.dkr.ecr.us-east-1.amazonaws.com/jarch:site-jarch-static-web
+
+docker push 193862077204.dkr.ecr.us-east-1.amazonaws.com/jarch:site-jarch-static-web
