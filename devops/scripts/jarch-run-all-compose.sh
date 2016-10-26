dfa9175b382e56b4e6a54c4ceb415de13cff5f01
Joe Rice <joericearchitect@gmail.com>
2016-10-25 20:20:07 -0400

    scirpt fixes

--- devops/scripts/jarch-run-all-compose.sh
+++ devops/scripts/jarch-run-all-compose.sh
@@ -13,10 +13,11 @@ source $DIR/setenv.sh
 
 cd $JARCH_STATIC_WEB_APP_CODE_DIR
 
-echo docker command -    docker-compose -f $JARCH_APPS_DIR/docker-compose.yml -f $JARCH_STATIC_WEB_APP_CODE_DIR/docker-compose.yml -f $JARCH_BLOG_WORDPRESS_APP_CODE_DIR/docker-compose.yml up -d
+echo command to start all sites      - docker-compose -f $JARCH_STATIC_WEB_APP_CODE_DIR/docker-compose.yml -f $JARCH_BLOG_WORDPRESS_APP_CODE_DIR/docker-compose.yml up -d
+echo command to start nginx          - docker-compose -f $INFRA_NGINX_PROXY_APP_DIR/docker-compose.yml
 
 # Start the nginx container first, since the others will need to register with it.
-docker-compose -f $INFRA_NGINX_PROXY_APP_DIR/docker-compose.yml
+docker-compose -f $INFRA_NGINX_PROXY_APP_DIR/docker-compose.yml up -d
 
 docker-compose -f $JARCH_STATIC_WEB_APP_CODE_DIR/docker-compose.yml -f $JARCH_BLOG_WORDPRESS_APP_CODE_DIR/docker-compose.yml up -d
 
