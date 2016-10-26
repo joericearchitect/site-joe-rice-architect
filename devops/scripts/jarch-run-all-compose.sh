ba8ec8112665cb837c93e6a4e27cb13b509101af
Joe Rice <joericearchitect@gmail.com>
2016-10-25 23:44:49 -0400

    script changes

--- devops/scripts/jarch-run-all-compose.sh
+++ devops/scripts/jarch-run-all-compose.sh
@@ -19,13 +19,13 @@ echo .
 echo ********************************************************************************
 echo * Starting up containers with the follwoing compose files:
 echo *   Nginx - $INFRA_NGINX_PROXY_APP_DIR/docker-compose.yml
-echo *   J-Arch Site - $JARCH_STATIC_WEB_APP_CODE_DIR/docker-compose-nginx.yml
-echo *   WordPress   - $JARCH_BLOG_WORDPRESS_APP_CODE_DIR/docker-compose-nginx.yml
-echo *   Drupal      - $JARCH_BLOG_DRUPAL_APP_CODE_DIR/docker-compose-nginx.yml
+echo *   J-Arch Site - $JARCH_STATIC_WEB_APP_CODE_DIR/docker-compose.yml
+echo *   WordPress   - $JARCH_BLOG_WORDPRESS_APP_CODE_DIR/docker-compose.yml
+echo *   Drupal      - $JARCH_BLOG_DRUPAL_APP_CODE_DIR/docker-compose.yml
 echo ********************************************************************************
 
-echo command to start all sites      - docker-compose -f $INFRA_NGINX_PROXY_APP_DIR/docker-compose.yml -f $JARCH_STATIC_WEB_APP_CODE_DIR/docker-compose-nginx.yml -f $JARCH_BLOG_WORDPRESS_APP_CODE_DIR/docker-compose-nginx.yml up -d
+echo command to start all sites      - docker-compose -f $INFRA_NGINX_PROXY_APP_DIR/docker-compose.yml -f $JARCH_STATIC_WEB_APP_CODE_DIR/docker-compose.yml -f $JARCH_BLOG_WORDPRESS_APP_CODE_DIR/docker-compose.yml up -d
 
-docker-compose -f $INFRA_NGINX_PROXY_APP_DIR/docker-compose.yml -f $JARCH_STATIC_WEB_APP_CODE_DIR/docker-compose-nginx.yml -f $JARCH_BLOG_WORDPRESS_APP_CODE_DIR/docker-compose-nginx.yml up -d
+docker-compose -f $INFRA_NGINX_PROXY_APP_DIR/docker-compose.yml -f $JARCH_STATIC_WEB_APP_CODE_DIR/docker-compose-nginx.yml -f $JARCH_BLOG_WORDPRESS_APP_CODE_DIR/docker-compose.yml -f $JARCH_BLOG_DRUPAL_APP_CODE_DIR/docker-compose.yml up -d
 
 #docker-compose -f $JARCH_STATIC_WEB_APP_CODE_DIR/docker-compose.yml -f $JARCH_BLOG_WORDPRESS_APP_CODE_DIR/docker-compose.yml -f $JARCH_BLOG_DRUPAL_APP_CODE_DIR/docker-compose.yml up -d
