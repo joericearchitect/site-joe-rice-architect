e082968aa8f033207ab1eceec056922c568a1603
Joe Rice <joericearchitect@gmail.com>
2016-11-10 18:53:49 -0500

    modifying docker file for wordpress blog so that it pulls in theme

--- devops/scripts/jarch-stop-all-apps.sh
+++ devops/scripts/jarch-stop-all-apps.sh
@@ -25,7 +25,9 @@ echo -   Drupal      - $JARCH_BLOG_DRUPAL_APP_CODE_DIR/docker-compose.yml
 echo --------------------------------------------------------------------------------
 
 
-$INFRA_SCRIPTS_DOCKER_DIR/stop-docker-container-compose.sh "Wordpress Blog" $INFRA_NGINX_PROXY_APP_DIR
-$INFRA_SCRIPTS_DOCKER_DIR/stop-docker-container-compose.sh "Wordpress Blog" $JARCH_STATIC_WEB_APP_CODE_DIR
-$INFRA_SCRIPTS_DOCKER_DIR/stop-docker-container-compose.sh "Wordpress Blog" $JARCH_BLOG_WORDPRESS_APP_CODE_DIR
-$INFRA_SCRIPTS_DOCKER_DIR/stop-docker-container-compose.sh "Wordpress Blog" $JARCH_BLOG_DRUPAL_APP_CODE_DIR
+docker-compose -f $INFRA_NGINX_PROXY_APP_DIR/docker-compose.yml -f $JARCH_STATIC_WEB_APP_CODE_DIR/docker-compose.yml -f $JARCH_BLOG_WORDPRESS_APP_CODE_DIR/docker-compose-dev.yml -f $JARCH_BLOG_DRUPAL_APP_CODE_DIR/docker-compose.yml up -d
+
+#$INFRA_SCRIPTS_DOCKER_DIR/stop-docker-container-compose.sh "Wordpress Blog" $INFRA_NGINX_PROXY_APP_DIR
+#$INFRA_SCRIPTS_DOCKER_DIR/stop-docker-container-compose.sh "Wordpress Blog" $JARCH_STATIC_WEB_APP_CODE_DIR
+#$INFRA_SCRIPTS_DOCKER_DIR/stop-docker-container-compose.sh "Wordpress Blog" $JARCH_BLOG_WORDPRESS_APP_CODE_DIR
+#$INFRA_SCRIPTS_DOCKER_DIR/stop-docker-container-compose.sh "Wordpress Blog" $JARCH_BLOG_DRUPAL_APP_CODE_DIR
